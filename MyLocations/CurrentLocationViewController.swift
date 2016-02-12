72efc2d22f995910cbd37f095f265ef85095c83f
Derek Pacula <derek@pacula.me>
2016-02-12 09:53:16 -0600

    Renamed View Controllers and removed initial labels Added descriptive labels to the UI Added Core Locations framework Added debugging messages for error in receiving locations, and updating locations

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -7,8 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
+
+
+
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +30,39 @@ class CurrentLocationViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func getLocation() {
+        
+        
+        let authStatus = CLLocationManager.authorizationStatus()
+        print("is this being called?")
+        
+        if authStatus == .NotDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            print("is authorization being called?")
+            return
+        }
+       
+        print("after the if")
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+      
+    }
+    
+    // MARK: - CLLocatoinManagerDelegate
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        
+        print("didFailWithError \(error)")
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let newLocation = locations.last!
+        print("didUPdateLocations \(newLocation)")
     }
 
 
